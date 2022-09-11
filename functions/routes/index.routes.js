d5828057033e5e9db52bd09f2c9aa65d47d7b9ed
Jehiverson <Jehivis@gmail.com>
2022-09-11 01:18:10 -0600

    Update: Second version to APIS

--- functions/routes/index.routes.js
+++ functions/routes/index.routes.js
@@ -8,9 +8,10 @@ const ourmission = require("./ourmission.routes");
 const clienttestimonials = require("./clienttestimonials.routes");
 const ourservices = require("./ourservices.routes");
 const aboutus = require("./aboutus.routes");
-const paymethods = require("./paymethod.routes");
+const sendmethods = require("./sendmethods.routes");
 const footer = require("./footer.routes");
 const location = require("./location.routes");
+const locationdescription = require("./locationdescription.routes");
 
 router.use('', carrousel);
 router.use('', products);
@@ -18,8 +19,9 @@ router.use('', ourmission);
 router.use('', clienttestimonials);
 router.use('', ourservices);
 router.use('', aboutus);
-router.use('', paymethods);
+router.use('', sendmethods);
 router.use('', footer);
 router.use('', location);
+router.use('', locationdescription);
 
 module.exports = router;
--- functions/routes/sendmethods.routes.js
+++ functions/routes/sendmethods.routes.js
@@ -6,31 +6,31 @@ const generalFunctions = require("../utils/generalFunction");
 const dbFunction = require('../utils/dbFunctions');
 const { getDataCollection, getDataCollectionForId, setDataCollection, updateDataCollection, deleteDataCollection } = dbFunction;
 
-router.get("/paymethods", async(req, res) => {
-   var data = await getDataCollection('PayMethods');
+router.get("/sendmethods", async(req, res) => {
+   var data = await getDataCollection('SendMethods');
    res.json({data});
 });
 
-router.get("/paymethods/:id", async(req, res) => {
+router.get("/sendmethods/:id", async(req, res) => {
    console.log(req.params.id);
-    var data = await getDataCollectionForId('PayMethods', req.params.id);
+    var data = await getDataCollectionForId('SendMethods', req.params.id);
     res.json({data});
  });
 
-router.post("/paymethods", async(req, res) => {
+router.post("/sendmethods", async(req, res) => {
     let idGenereted = generalFunctions.randomId(req.body.title, req.body.description);
     req.body.id = idGenereted;
-    await setDataCollection('PayMethods', idGenereted, req.body);
+    await setDataCollection('SendMethods', idGenereted, req.body);
     res.json({status: 1});
  });
 
- router.patch("/paymethods", async(req, res) => {
-    var dataUpdate = await updateDataCollection('PayMethods', req.body.id, req.body);
+ router.patch("/sendmethods", async(req, res) => {
+    var dataUpdate = await updateDataCollection('SendMethods', req.body.id, req.body);
     res.json({status: dataUpdate});
  });
 
- router.delete("/paymethods", async(req, res) => {
-    var dataDelete = await deleteDataCollection('PayMethods', req.body.id);
+ router.delete("/sendmethods", async(req, res) => {
+    var dataDelete = await deleteDataCollection('SendMethods', req.body.id);
     res.json({status: dataDelete});
  });
  
